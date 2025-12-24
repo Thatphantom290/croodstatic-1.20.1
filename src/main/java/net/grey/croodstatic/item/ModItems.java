8123791792a3f7111b89cb7d4b2a80fa2ce196f3
Thatphantom290 <thatphantomguthub@gmail.com>
2025-12-24 19:39:47 -0300

    oh god, how many commits today?

--- src/main/java/net/grey/croodstatic/item/ModItems.java
+++ src/main/java/net/grey/croodstatic/item/ModItems.java
@@ -1,16 +1,11 @@
 package net.grey.croodstatic.item;
 
 import net.grey.croodstatic.Croodstatic;
-import net.grey.croodstatic.block.ModBlocks;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
-import static net.grey.croodstatic.block.ModBlocks.MOIST_GRASS;
-import static net.minecraftforge.registries.ForgeRegistries.ITEMS;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -18,10 +13,6 @@ public class ModItems {
 
     public static final RegistryObject<Item> CHICKUNA_EGG = ITEMS.register("chickuna_egg",
             () -> new Item(new Item.Properties()));
-
-    public static final RegistryObject<Item> MOIST_GRASS_ITEM = ITEMS.register("moist_grass",
-            () -> new BlockItem(MOIST_GRASS.get(), new Item.Properties()));
-
 
 
     public static void register(IEventBus eventBus) {
