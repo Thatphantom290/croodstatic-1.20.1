6be54e4739ef6a21a8d19ad315552f1d740c1e26
Thatphantom290 <thatphantomguthub@gmail.com>
2025-12-25 06:06:46 -0300

    better work now

--- src/main/java/net/grey/croodstatic/client/renderer/WildChickunaRenderer.java
+++ src/main/java/net/grey/croodstatic/client/renderer/WildChickunaRenderer.java
@@ -1,11 +1,11 @@
 package net.grey.croodstatic.client.renderer;
 
 import net.grey.croodstatic.client.model.WildChickunaModel;
-import net.grey.croodstatic.entity.ChickunaEntity;
+import net.grey.croodstatic.entity.WildChickunaEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
-public class WildChickunaRenderer extends GeoEntityRenderer<ChickunaEntity> {
+public class WildChickunaRenderer extends GeoEntityRenderer<WildChickunaEntity> {
     public WildChickunaRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new WildChickunaModel());
     }
